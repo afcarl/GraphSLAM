9f0002e1c004f12b7d13c1ec45421e52e920f08c
Brendon Forsgren <bforsgren29@gmail.com>
2018-06-15 12:09:51 -0600

    Start of the GraphSLAM algorithm. I think everything can go in this file.

---
+++ slam.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def main():
+    print 'Starting GraphSLAM'
+
+
+    print 'Finished'
+
+if __name__=="__main__":
+    main()
